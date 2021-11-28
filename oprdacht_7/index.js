8737962b7b2f0f34b4f31cc334a35a018e366605
mwaalkens <92387442+mwaalkens@users.noreply.github.com>
2021-11-28 14:11:05 +0100

    Add files via upload

---
+++ oprdacht_7/index.js
@@ -0,0 +1,10 @@
+
+console.log("Hello Winc Academy");
+let firstName = 'Marlies';
+console.log(firstName);
+firstName = 4 + 4;
+firstName = 5 % 2;
+firstName = 48;
+console.log(typeof (firstName));
+firstName = '48';
+console.log(typeof (firstName));
