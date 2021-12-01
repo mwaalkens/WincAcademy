3169a19516d437818c3a99fce00d04601ccca354
mwaalkens <92387442+mwaalkens@users.noreply.github.com>
2021-12-01 21:04:48 +0100

    Create script.js

---
+++ opdracht_13/script.js
@@ -0,0 +1,7 @@
+const paintWall = function (wall, colour) {
+    console.log('The ' + wall + ' wall had been painted ' + colour);
+}
+
+paintWall('north', 'red');
+paintWall('south-east', 'green');
+paintWall();
