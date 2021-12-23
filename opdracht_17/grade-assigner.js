039db57c7ff8438316b4ed0db48f8a4810891a28
mwaalkens <92387442+mwaalkens@users.noreply.github.com>
2021-12-23 14:24:15 +0100

    Create grade-assigner.js

---
+++ opdracht_17/grade-assigner.js
@@ -0,0 +1,17 @@
+const assignGrade = function(score) {
+  if (score > 90) {
+    return "A";
+  } else if (score > 80) {
+    return "B";
+  } else if (score > 70) {
+    return "C";
+  } else if (score > 65) {
+    return "D";
+  } else {
+    return "E";
+  }
+}
+
+for (i=60; i<=100; i=i++){
+  console.log("For scoring " + i + " points, you get an " + assignGrade(i))
+}
