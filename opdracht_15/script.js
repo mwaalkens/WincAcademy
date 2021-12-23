7678d427430bb4776d52af0ee9993145186b65f7
mwaalkens <92387442+mwaalkens@users.noreply.github.com>
2021-12-23 13:23:22 +0100

    Create script.js

---
+++ opdracht_15/script.js
@@ -0,0 +1,27 @@
+/* function mathSquare(number1, number2){
+    let square1 = number1*number1;
+    let square2 = number2*number2;
+    let sum = square1 + square2;
+    let square3= Math.pow(sum);
+    return square1;
+} */
+/* 
+const mathSquare = function(number1, number2){
+    let square1 = number1*number1;
+    let square2 = number2*number2;
+    let sum = square1 + square2;
+    let square3= Math.pow(sum);
+    return square1;
+}
+ */
+
+
+const mathSquare = (number1, number2) => {
+    let square1 = number1*number1;
+    let square2 = number2*number2;
+    let sum = square1 + square2;
+    let square3= Math.pow(sum);
+    return square1;
+}
+
+console.log(mathSquare(112,43));
