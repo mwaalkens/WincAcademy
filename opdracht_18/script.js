9aebfa45fa65d22be304226a64ab673a4dfa4bb8
mwaalkens <92387442+mwaalkens@users.noreply.github.com>
2021-12-23 14:37:22 +0100

    Create script.js

---
+++ opdracht_18/script.js
@@ -0,0 +1,15 @@
+let colors = ['yellow', 'blue', 'red', 'orange']
+
+
+// let i = 0;
+// while (i<colors.length) {
+//     console.log(colors[i]);
+//     i++;
+// }
+
+
+// for (i=0; i<colors.length; i++){
+//     console.log(colors[i]);
+// }
+
+colors.forEach(element => console.log(element));
